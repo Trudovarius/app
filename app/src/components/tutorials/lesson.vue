9cfbf4d6faf986433cdce3a27ac0ea23c6e49fe9
Trudovarius <rudi1217@gmail.com>
2020-02-05 20:38:08 +0100

    Added jquery, lesson.vue element displays console output

--- app/src/components/tutorials/lesson.vue
+++ app/src/components/tutorials/lesson.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="lesson">
+    <div class="task">
+    </div>
     <div class="canvas">
       <canvas></canvas>
     </div>
     <div class="code">
     </div>
-
+    <div class="console">
+    </div>
   </div>
 </template>
 
@@ -28,6 +31,21 @@
     mounted(){
       if (!this.auth) {
         this.$router.push("/signin");
+      } else {
+        $(".console").empty();
+        var former = console.log;
+        console.log = function(msg){
+            // former(msg);  //maintains existing logging via the console.
+            $(".console").append("<div class='line'>" + msg + "</div>");
+            $(".console").scrollTop($(".console")[0].scrollHeight);
+        }
+
+        window.onerror = function(message, url, linenumber) {
+            console.log("JavaScript error: " + message + " on line " +
+                    linenumber + " for " + url);
+        }
+
+        console.log('XDDD')
       }
     }
   }
